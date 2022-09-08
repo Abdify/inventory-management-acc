42af7f5eb1e69f148391d301081c728d8bf1145b
abedinforhane <farhanengn@gmail.comm>
2022-09-08 12:38:55 +0600

    Model

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const DBConnect = require("./utils/dbConnect");
+
 
 const app = require("./app");
 
 // database connection
-DBConnect();
+mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
+  console.log(`Database connection is successful 🛢` .red.bold);
+})
+
 
 // server
 const port = process.env.PORT || 8080;
@@ -14,4 +17,5 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`.yellow.bold);
 });
+
 
